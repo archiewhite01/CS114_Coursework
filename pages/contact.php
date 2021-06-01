36ba3021745bd8d4f3693e023a3b0fb88b8aa91d
Archie <archie.white.2020@uni.strath.ac.uk>
2021-06-01 13:36:49 +0100

    initial website setup

---
+++ pages/contact.php
@@ -0,0 +1,44 @@
+<html>
+    <!--includes the header html in the page.-->
+    <head>
+        <?php include '../html/header.html'; ?>
+    </head>
+    <body>
+        <?php
+        if($_POST) {
+            $name = $_POST['name'];
+            $email = $_POST['email'];
+            $words = $_POST['message'];
+
+            $to = "archiewhite1@gmail.com";
+            $subject = "New message from ".$name."";
+            $txt = "FROM: ".$email."\n\n".$words."";
+            $headers = "From: webmaster@example.com" . "\r\n" .
+                "CC: ".$email."";
+
+            mail($to, $subject, $txt, $headers);
+            echo "message sent!";
+
+
+
+        }
+        ?>
+        <h3 style="text-align: center">Be reassured in the fact that we probably won't reply to you!</h3>
+
+        <div class="cell">
+            <form method="post">
+                <label for="name">Name</label>
+                <input type="text" id="name" name="name" placeholder="Enter your name..">
+
+                <label for="email address">Email Address</label>
+                <input type="text" id="email" name="email" placeholder="Enter your email address...">
+
+                <label for="message">Message</label>
+                <textarea id="message" name="message" placeholder="Your message here..." style="height:200px"></textarea>
+
+                <input type="submit" value="Submit">
+
+            </form>
+        </div>
+    </body>
+</html>
---
+++ pages/news.php
@@ -0,0 +1,52 @@
+<html>
+<!--includes the header html in the page. as well as an extra subheading.-->
+<head>
+        <?php include '../html/header.html'; ?>
+</head>
+<body>
+
+<section style="border-left: 20rem;">
+    <article>
+        <h3>"Don’t listen to my ex, he's crazy" says Johnson</h3>
+        <img src="../images/cummings.jpg" alt="Big dom." width="80%">
+        <p>Boris Johnson has told the British public that his ex is crazy and they should not believe any of his made-up stories.</p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>Man does charity work and doesn't tell anyone</h3>
+        <img src="../images/charity.jpg" alt="charity work" width="70%">
+        <p>A man from Durham has worked in a charity for 3 days and resisted the urge to post a picture of himself on facebook with the caption "charity work done" and a tick emoji. Local Police are investigating the incident.</p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>Harry Potter Books 'All Made Up' Says Rowling</h3>
+        <img src="../images/wizard.jpg" alt="Wizard man" width="70%">
+        <p>AUTHOR JK Rowling has revealed that no one is going to die at the end of the final Harry Potter book because she "made the whole thing up".</p>
+        <a>Read more...</a>
+    </article>
+</section>
+<section>
+    <article>
+        <h3>Amazon still refuses to refund parcel</h3>
+        <img src="../images/fight.jpg" alt="the brawl" width="70%">
+        <p>After a video showing a pack of hermes delivery drivers carrying out an unprovoked attack on a parcel containing a stuffed rabbit has gone viral, angered toddler says he'll "take them to the cleaners".</p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>Hung Parliament</h3>
+        <img src="../images/zoom.jpg" alt="keep your camera off." width="70%">
+        <p>A Canadian MP has been left red-faced after appearing naked during a virtual parliament meeting of the Canadian House of Commons AGAIN. </p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>The Father, The Son and The Heavy Spirits</h3>
+        <img src="../images/wine.jpg" alt="holy spirits" width="80%">
+        <p>A Rotherham woman has been banned from the Christian faith following how plastered she got after one too many sacramental wines.</p>
+        <a>Read more...</a>
+    </article>
+</section>
+
+<footer>
+    <p></p>
+</footer>
+</html>
