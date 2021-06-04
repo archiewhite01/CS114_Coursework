57e0a4fac9c829fe1477f41ebfcada1b11834f7e
Karina <karina.deptula.2020@uni.strath.ac.uk>
2021-06-04 08:06:24 +0100

    Weather and About pages

---
+++ about.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About us</title>
+    <link rel="stylesheet" href="../stylesheetK.css">
+    <?php include '../html/header.html'; ?>
+</head>
+<body>
+
+<div class="header">
+    <h1>About us</h1>
+    <p>Learn a bit about why we do what we do</p>
+</div>
+<div class="row">
+    <div class="column">
+        <div class="card">
+            <img src="images/story.jpg" alt="aboutpg" style="width:100%">
+            <div class="container">
+                <h2>Our History</h2>
+                <p>Here at BoringNews Corp, we started a small website that was run from the dining room of a little London flat.
+                    That has not changed, we just write an awful lot more. In fact in 2020 everyone had to do the same.
+                    Thanks for reading this I guess and for making our website what it is. We will continue to work
+                    smart not hard! </p>
+            </div>
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="card">
+            <img src="images/mission.jpg" alt="aboutpg" style="width:100%">
+            <div class="container">
+                <h2>Our Mission</h2>
+                <p> To bring relatively current news, weather and sports in one convenient place that will probably not
+                    bother reading. But it's the thought that counts and it's there should you need it. We try to be
+                    good at what we do. If you have any feedback about our website let us know via the contact page.
+                    We'll let you know if we like it or not and we might even publish it if we do.</p>
+            </div>
+        </div>
+    </div>
+
+    <div class="column">
+        <div class="card">
+            <img src="images/success.jpeg" alt="aboutpg" style="width:100%">
+            <div class="container">
+                <h2>Careers</h2>
+                <p>Are you a passionate writer? Would you like to work with an amazing team of reporters writing about
+                    the weather, sporting events or general news happening worldwide? If you answered yes then we probably
+                    have a good job for you. Send your CVs to wedontreply@BoringNewsCorp.com
+                    and we will aim to get back to you within a working month.
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+<?php include '../html/footer.html'; ?>
+</body>
+</html>
---
+++ weather.php
@@ -0,0 +1,147 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Weather</title>
+    <link rel="stylesheet" href="../stylesheetK.css">
+    <?php include '../html/header.html'; ?>
+</head>
+<body>
+
+<div class="header">
+    <h1>Weather Report</h1>
+    <p>See a 7 day forecast for the UK(its very accurate)</p>
+</div>
+<table>
+    <h1>London</h1>
+    <tr>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+        <th>Saturday</th>
+        <th>Sunday</th>
+
+    </tr>
+    <tr>
+        <td>12°</td>
+        <td>12°</td>
+        <td>12°</td>
+        <td>12°</td>
+        <td>-3°</td>
+        <td>40°</td>
+        <td>12°</td>
+    </tr>
+    <tr>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>Snow</td>
+        <td>Heatwave</td>
+        <td>RAIN</td>
+    </tr>
+
+</table>
+
+<table>
+    <h1>Cardiff</h1>
+    <tr>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+        <th>Saturday</th>
+        <th>Sunday</th>
+
+    </tr>
+    <tr>
+        <td>12°</td>
+        <td>12°</td>
+        <td>39°</td>
+        <td>12°</td>
+        <td>-5°</td>
+        <td>12°</td>
+        <td>12°</td>
+    </tr>
+    <tr>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>Heatwave</td>
+        <td>RAIN</td>
+        <td>Snow</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+    </tr>
+
+</table>
+
+<table>
+    <h1>Edinburgh</h1>
+    <tr>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+        <th>Saturday</th>
+        <th>Sunday</th>
+
+    </tr>
+    <tr>
+        <td>35°</td>
+        <td>12°</td>
+        <td>12°</td>
+        <td>0°</td>
+        <td>12°</td>
+        <td>12°</td>
+        <td>12°</td>
+    </tr>
+    <tr>
+        <td>Heatwave</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>Hail</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+    </tr>
+
+</table>
+<table>
+    <h1>Dublin</h1>
+    <tr>
+        <th>Monday</th>
+        <th>Tuesday</th>
+        <th>Wednesday</th>
+        <th>Thursday</th>
+        <th>Friday</th>
+        <th>Saturday</th>
+        <th>Sunday</th>
+
+    </tr>
+    <tr>
+        <td>12°</td>
+        <td>-10°</td>
+        <td>12°</td>
+        <td>12°</td>
+        <td>12°</td>
+        <td>12°</td>
+        <td>37°</td>
+    </tr>
+    <tr>
+        <td>RAIN</td>
+        <td>Blizzard</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>RAIN</td>
+        <td>Heatwave</td>
+    </tr>
+
+</table>
+<?php include '../html/footer.html'; ?>
+</body>
+</html>
