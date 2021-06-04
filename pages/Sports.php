5fc5b82961962cfa0bbbbb9eb5b8963cb8530070
wsb20182 <ewan.hughes.2020@uni.strath.ac.uk>
2021-06-04 12:11:02 +0100

    committing to Github

---
+++ pages/Sports.php
@@ -0,0 +1,53 @@
+<html>
+<!--includes the header html in the page. as well as an extra subheading.-->
+<head>
+    <?php include '../html/header.html'; ?>
+</head>
+<body>
+
+<section style="border-left: 25rem;">
+    <article>
+        <h3>Could Southgate's daring plan to field a starting XI of right backs be enough to win the Euros?</h3>
+        <img src="../images/Southgate.jpg" alt="Big Gaz" width="80%">
+        <p>Gareth Southgate has today announced he is dropping all of the announced midfielders and attackers from the latest England squad and plans to call up an additional 12 Right-backs</p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>Harry Kane quoted saying "I love Shpurs" hours before announcing his intended departure from the club</h3>
+        <img src="../images/HarryKane.jpg" alt="Hazza K" width="70%">
+        <p>Yound Spurs fan filmed Harry Kane leaving the new Tottenham Hotspur Stadium claiming he loves the club before driving straight to his agents house to discuss a move from his Club</p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>Golf star Rory Mcilroy claims he "doesn't even really like golf" despite tremendous success</h3>
+        <img src="../images/Rory.jpg" alt="Rory" width="70%">
+        <p>Rory Mcilroy has today told a reporter he doesn't even like the sport that has consumed his life for over 30 years, claiming that "you can't turn down the boat load of money that comes with the success"</p>
+        <a>Read more...</a>
+    </article>
+</section>
+<section>
+    <article>
+        <h3>National league side Wealdstone taken over by the famous Wealdstone Raider</h3>
+        <img src="../images/Wealdstoneraider.jpg" alt="Hooligan" width="70%">
+        <p>The Raider has been pictured at the stadium today to announce his tremendous takeover saying he got the money by trademarking the phrase "you've got no fans"</p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>Lewis Hamilton claims he was determined to be the most decorated formula 1 driver in history ever since his Primary school friend called him "stinky head"</h3>
+        <img src="../images/LewisHamilton.jpg" alt="Hamilton" width="70%">
+        <p>Current F1 champion has opened up about his struggles through primary school as he tells us how he used to take his go kart to school to practice his driving. He also says the recent musical 'Hamilton' is about his life story. We don't think he's seen it</p>
+        <a>Read more...</a>
+    </article>
+    <article>
+        <h3>Rugby Star says he is quitting the sport to follow his dream of working with the ladies who knit all the shreddies</h3>
+        <img src="../images/Knitted.jpg" alt="Shreddies" width="80%">
+        <p>A Rugby Union Star has today announced he is giving up his professional career to "join the ladies who knit all the Shreddies" in an attempt to star in the advert. Doctors are assessing him for a potential concussion.</p>
+        <a>Read more...</a>
+    </article>
+</section>
+
+<footer>
+    <p></p>
+</footer>
+</html><?php
+
